f6b51c0a5aa0c7b01890477ad44986d74246e12a
MaFernandaLopeZ <maferlz265@gmail.com>
2022-11-24 22:17:15 -0600

    add polymorphism project with interfaces v1 and v2

---
+++ Semana 1/Interface/src/com/hospital/Doctor.java
@@ -0,0 +1,34 @@
+package com.hospital;
+
+public class Doctor implements Empleados {
+	
+	protected String nombre;
+	protected double sueldo_mensual;
+	protected int dias_laborados;
+	protected String especialidad;
+	
+	public Doctor(String nombre, double sueldo_mensual, int dias_laborados, String especialidad) {
+		this.nombre = nombre;
+		this.sueldo_mensual = sueldo_mensual;
+		this.dias_laborados = dias_laborados;
+		this.especialidad = especialidad;
+	}
+	
+	@Override
+	public double calcular_sueldoQuincenal(){
+		return (sueldo_mensual/30)*dias_laborados;
+	}
+	
+	public String toString() {
+		return "Datos administrativos: \nEspecialidad:"+especialidad+"\nSueldo mensual: "+sueldo_mensual+"\nDias laborados: "+dias_laborados;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ Semana 1/Interface/src/com/hospital/Empleados.java
@@ -0,0 +1,7 @@
+package com.hospital;
+
+public interface Empleados {
+	
+	double calcular_sueldoQuincenal();
+	String getNombre();
+}
