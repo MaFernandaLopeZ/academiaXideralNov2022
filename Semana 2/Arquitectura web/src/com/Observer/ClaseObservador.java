54f927c3d64944fabe1a1c12e02e3332398996a1
MaFernandaLopeZ <maferlz265@gmail.com>
2022-12-02 09:52:57 -0600

    add mvc patterns

---
+++ Semana 2/Arquitectura web/src/com/Observer/ClaseObservador.java
@@ -0,0 +1,37 @@
+package com.Observer;
+
+import java.util.Observable;
+
+
+/**
+ * Clase observador donde se establece el eje central del patron
+ */
+
+public class ClaseObservador extends Observable{
+	
+	private int colorSeleccionado;
+	private String color;
+	
+	public ClaseObservador(){
+		
+	}
+		
+	public void setColorSeleccionado(int i) {
+		this.colorSeleccionado = i;
+		setChanged();
+	    notifyObservers();
+	}
+
+	public int getColorSeleccionado() {
+		return colorSeleccionado;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+}
